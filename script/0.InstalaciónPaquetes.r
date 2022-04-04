b8333ef7e18ff7f1b68bfc09823a92efcdb2df9e
josezea <jfzeac@unal.edu.co>
2022-04-04 09:05:50 -0500

    Add files via upload

---
+++ script/0.InstalaciónPaquetes.r
@@ -0,0 +1,18 @@
+
+# Instalación de R
+https://cloud.r-project.org/
+  
+  Instalación de Rstudio
+https://www.rstudio.com/products/rstudio/download/
+  
+install.packages(haven) # Leer archivos de SPSS
+install.packages(vroom) # Paquete para acelelar la lectura de archivos de texto
+install.packages(labelled) # Leer etiquetas de los archivos de SPSS
+install.packages(dplyr)
+install.packages(arrow)
+install.packages(srvyr) # Muestreo, estimaciones de proporciones, razones y estadísticas con sus medidas de error 
+install.packages(samplesize4surveys) # Correlación intraclásica, tablas de muestreo
+
+
+# Para edición de tablas
+install.packages(flextable)
